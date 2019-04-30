5d117f509f9a7de929b26d27ce2115852ec08eb2
ahmedspring2020 <50028835+ahmedspring2020@users.noreply.github.com>
2019-05-01 01:35:18 +0530

    new files added

---
+++ server/model/UserPlayListModel.js
@@ -0,0 +1,32 @@
+"use strict";
+/**
+ * @description This Model is used for storing login credentials
+ * @author Ahmed
+ * @since MAR-28-2019
+ *
+ */
+
+var mongoose = require("mongoose");
+
+var userPlayListSchema = mongoose.Schema({
+  userId: {
+    type: String,
+    required: true
+  },
+  dayNo: Number,
+  mediaId: String,
+  create_date: {
+    type: Date
+  },
+});
+// Export Contact model
+var UserPlayList = (module.exports = mongoose.model(
+  "userPlayList",
+  userPlayListSchema
+));
+
+
+
+module.exports.get = function(callback, limit) {
+  UserPlayList.find(callback).limit(limit);
+};
