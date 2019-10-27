c22853c835180bfa039de16bae3ce1f76dcf2d45
Shoeb Mirza <Shoeb.Mirza@jdplc.com>
2019-10-27 17:42:03 +0530

    no message

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-const serverUrl = 'http://localhost:8080/';
+const serverUrl = 'http://cms.muzmind.com/';
 const loginUrl = serverUrl + 'auth';
 const baseUrl = serverUrl + 'cms';
 const uploadUrl = serverUrl + 'upload';
