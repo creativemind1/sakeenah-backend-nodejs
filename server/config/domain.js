b4d682b1d0eb37f51c322741907ee6488c6883ef
Shoeb Mirza <shoeb.mirza@xitsolutionsinc.com>
2026-05-17 13:22:28 +0530

    committing

--- server/config/domain.js
+++ server/config/domain.js
@@ -1,5 +1,5 @@
-exports.domain = function() {
-    let IP = '127.0.0.1';
-    let PORT = '8080'
-    return 'http://'+IP+':'+PORT;
+exports.domain = function () {
+    let IP = process.env.IP || '127.0.0.1';
+    let PORT = process.env.PORT || '8080'
+    return 'http://' + IP + ':' + PORT;
 }
